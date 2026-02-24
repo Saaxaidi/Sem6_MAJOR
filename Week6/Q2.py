4efc315dea237f1bed2466d9ac838f4a2b77b884
Syeda Aleema Abbas Zaidi <alimaabbaszaidi100@gmail.com>
2026-02-24 14:20:49 +0530

    final commit

--- Week6/Q2.py
+++ Week6/Q2.py
@@ -2,7 +2,6 @@ t1 = float(input("Enter the temp for probes 1:  "))
 t2 = float(input("Enter the temp for probes 2:  "))
 t3 = float(input("Enter the temp for probes 3:  "))
 
-
 if(t1<t2) and (t1<t3):
     print(t1 ," is the lowest temperature")
 elif(t2<t1) and (t2<t1):
--- Week6/Q8.py
+++ Week6/Q8.py
@@ -1,9 +1,6 @@
 data = eval(input("Enter a list: "))
-
 total = 0
-
 for item in data:
     if isinstance(item, int):
         total += item
-
-print("Sum of integers:", total)
+print(total)
